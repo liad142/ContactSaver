09b4aae549ce0a71800ffcd54b2f9a6d00e9c20b
liad <liad142@gmail.com>
2021-07-08 11:43:14 +0300

    prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 
@@ -20,6 +21,16 @@ app.use('/api/users',require('./routes/users'));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/contacts',require('./routes/contacts'));
 
+//serve static asses it production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'))
+
+    //__DIRNAME תסתכל על התיקייה הנוכחית
+    //ואז תסתכל על קליינט - בילד - אינדקס HTML
+    app.get('*',(req,res)=>
+    res.sendFile(path.resolve(__dirname,'client','build','index.html')))
+}
 
 const PORT =process.env.PORT || 5000
 
